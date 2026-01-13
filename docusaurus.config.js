34a28e9b5623643d300f8cbde8da8f0ed1d96812
ShopskaSalad <akinkamilov@gmail.com>
2026-01-13 13:41:59 +0300

    vercel fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   organizationName: 'iyzico',
   projectName: 'docs',
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internalization, you can use this field to set useful
